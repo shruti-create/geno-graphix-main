5df978fb89e39298c4b8c95866e3bef87874d203
Uliyaah Dionisio <uliyaahd@gmail.com>
2024-02-03 09:51:07 -0800

    fixed positioning of magnification portion

--- frontend/src/components/MagnifiedBox.js
+++ frontend/src/components/MagnifiedBox.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./MagnifiedBox.css";
 
 const MagnifiedBox = ({ sequence }) => {
+
   return (
     <div>
-    <p style={{marginRight: '70px'}}> Magnify a portion of your sequence and associated labels in this box! 
-        Highlight the part of the sequence that you want to magnify from the box on the left. </p>
+      <p style={{ marginRight: "70px" }}>
+        {" "}
+        Magnify a portion of your sequence and associated labels in this box!
+        Highlight the part of the sequence that you want to magnify from the box
+        on the left.{" "}
+      </p>
       <div className="box">
-        {sequence}
+      <pre>{sequence}</pre>
       </div>
     </div>
   );
