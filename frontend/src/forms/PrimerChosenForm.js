0a9c8ea806fadd87a6fa6fdc24d1d721f3184a07
shruti-create <shruti.bhamidipati@gmail.com>
2024-02-16 19:24:41 -0800

    basic primer design implemented

--- frontend/src/forms/PrimerChosenForm.js
+++ frontend/src/forms/PrimerChosenForm.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function PrimerShowPage(inputtedSequence) {
-    const [manipulatedSequence, setManipulatedSequence] = useState('');
+    const [forwardPrimers, setForwardPrimers] = useState([]);
+    const [reversePrimers, setReversePrimers] = useState([]);
+
     const fetchManipulatedSequence = async () => {
         try {
             const response = await axios.post('http://127.0.0.1:5000/primer', {
                 sequence: inputtedSequence
             });
-            setManipulatedSequence(response.data.manipulatedSequence.input);
+            setForwardPrimers(response.data.allPrimers[0]);
+            setReversePrimers(response.data.allPrimers[1]);
         } catch (error) {
             console.error('Error fetching manipulated sequence', error);
         }
@@ -21,7 +24,9 @@ function PrimerShowPage(inputtedSequence) {
     return (
         <div>
             <p style={{ fontSize: '1.2rem', color: 'black', margin: '20px', overflowWrap: 'break-word', overflow: 'hidden', textOverflow: 'ellipsis'}}>
-                Manipulated Sequence: {manipulatedSequence}
+                Forward Primers: {forwardPrimers}
+                <br/>
+                Reverse Primers: {reversePrimers}
             </p>
         </div>
     );
