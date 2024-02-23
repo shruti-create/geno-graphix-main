7c77706246f59a5b6e4735a07a1b5de7d61aa93a
shruti-create <shruti.bhamidipati@gmail.com>
2024-02-23 11:50:49 -0800

    change by hex

--- frontend/src/components/Annotations.js
+++ frontend/src/components/Annotations.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faToolbox } from '@fortawesome/free-solid-svg-icons';
 
 // AnnotationBox component handles annotation type selection.
 const AnnotationBox = ( {onAnnotationSelect}) => {
+    const [color, setColor] = useState('#ffffff'); 
     return (
     <div>
       <p>Pick an annotation type to do on the sequence:</p>
@@ -12,7 +13,8 @@ const AnnotationBox = ( {onAnnotationSelect}) => {
               <FontAwesomeIcon icon={faToolbox} />
           </div>
           <div>
-              <button onClick={() => onAnnotationSelect('Highlight')}>Highlight</button>
+              <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+              <button onClick={() => onAnnotationSelect(color)}>Highlight</button>
               <button onClick={() => onAnnotationSelect('Underline')}>Underline</button>
           </div>
       </div>
