68c5a692822568ee659ad4d57f8bcc1a4de957e3
shruti-create <shruti.bhamidipati@gmail.com>
2024-02-27 09:53:35 -0800

    primer display

--- frontend/src/forms/PrimerChosenForm.js
+++ frontend/src/forms/PrimerChosenForm.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 function PrimerShowPage(inputtedSequence) {
     const [forwardPrimers, setForwardPrimers] = useState([]);
     const [reversePrimers, setReversePrimers] = useState([]);
-
+    const [mainString, setMainString] = useState("");
     // Fetches manipulated sequence from the server
     const fetchManipulatedSequence = async () => {
         try {
@@ -17,6 +17,7 @@ function PrimerShowPage(inputtedSequence) {
             // Update state with forward and reverse primers
             setForwardPrimers(response.data.allPrimers[0]);
             setReversePrimers(response.data.allPrimers[1]);
+            showSequence();
         } catch (error) {
             console.error('Error fetching manipulated sequence', error);
         }
@@ -29,9 +30,20 @@ function PrimerShowPage(inputtedSequence) {
     
     console.log("Forward "+ forwardPrimers)
 
+    function showSequence(){
+        var length = inputtedSequence.input.length;
+        var temp = "";
+        for(var i = 0; i<length; i++){
+            temp = temp + "-";
+        }
+        setMainString(temp);
+    }
     // JSX for rendering the component
     return (
         <div style={{ padding: '1%', overflowY: 'scroll', height: '65vh', borderRadius: '1vh', borderColor: 'purple', borderWidth: '0.02vh', borderStyle: 'solid',}}>
+            <div style= {{padding: "1%", borderRadius: '1vh', borderStyle:'solid', borderColor: 'black', height: '40vh', overflow: 'auto',whiteSpace: 'pre-wrap' }}>
+                {mainString}
+            </div>
             <p style={{ fontSize: '1.2rem', color: 'black', margin: '0.5%', marginTop: '0',  overflowWrap: 'break-word', overflow: 'hidden', textOverflow: 'ellipsis'}}>
                 <h4>Forward Primers:</h4>
                 <hr style = {{marginTop:'1%', marginBottom: '2%'}}/>
