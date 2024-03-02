00a1c42e14013e7839174e3d1adc4102a341aada
Uliyaah Dionisio <uliyaahd@gmail.com>
2024-03-01 19:53:10 -0800

    Fixed selection sequence

--- frontend/src/components/fullSequence.js
+++ frontend/src/components/fullSequence.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./FullSequence.css"; // Import the CSS file for styling
 
 // FullSequence component displays the entire DNA sequence in a resizable box.
-// It allows users to select a portion of the sequence, logs the selection, 
+// It allows users to select a portion of the sequence, logs the selection,
 // and sends the selected text along with start and end indices back to the parent component.
 const FullSequence = ({ sequence, onSequenceSelect, annotations }) => {
   const [formattedAnnotations, setFormattedAnnotations] = useState([]);
@@ -27,103 +27,128 @@ const FullSequence = ({ sequence, onSequenceSelect, annotations }) => {
     // console.log("Formatted annotation: ", formatted);
   }, [annotations]);
 
-
   // TODO: Fix case when overlapping with existing annotation
   // TODO: Fix case when annotation button is clicked multiple times on the same selection
 
   const renderSequenceWithAnnotations = () => {
     let result = [];
     let lastIndex = 0;
-  
+    let lastType = '';
+
     // Sort the annotations by start index in ascending order
-    const sortedAnnotations = formattedAnnotations.sort((a, b) => a.start - b.start);
-  
+    const sortedAnnotations = formattedAnnotations.sort(
+      (a, b) => a.start - b.start
+    );
+
     sortedAnnotations.forEach(({ start, end, type }, index) => {
       const annotatedPart = sequence.slice(start, end);
-  
+      if ( start <= lastIndex){  // Current annotation is start in previous annotation
+        result.pop();
+        if (end < lastIndex){   // If the current annotation is in the middle the previous annotation
+          result.push(
+            <span
+              key={index`-overlap`}
+              style={{
+                backgroundColor: lastType[0] === "#" ? lastType : undefined,
+                borderBottom:
+                  lastType === "Underline" ? "2px solid #634c89f0" : undefined,
+                fontWeight: lastType === "Bold" ? "bold" : undefined,
+                textDecoration:
+                  lastType === "StrikeThrough" ? "line-through" : undefined,
+              }}
+              className="sequence"
+            >
+              {sequence.slice(sortedAnnotations[index-1].end, start)}
+            </span>
+          )
+          result.push(
+            <span
+              key={index`-overlap`}
+              style={{
+                backgroundColor: type[0] === "#" ? type : undefined,
+                borderBottom:
+                type === "Underline" ? "2px solid #634c89f0" : undefined,
+                fontWeight: type === "Bold" ? "bold" : undefined,
+                textDecoration:
+                type === "StrikeThrough" ? "line-through" : undefined,
+              }}
+              className="sequence"
+            >
+              {sequence.slice(start, end)}
+            </span>
+          )
+        }
+        else{
+          if(lastType === type){
+            return;
+          }
+          else{
+
+          }
+        }
+      }
+
       // Add the unannotated part of the sequence
       result.push(sequence.slice(lastIndex, start));
       // Add the annotated part with appropriate styling
-      if (type[0] === "#") {
-        result.push(
-          <span
-            key={index}
-            style={{ backgroundColor: type }}
-            className='sequence'
-          >
-            {annotatedPart}
-          </span>
-        );
-      } else if (type === "Underline") {
-        result.push(
-          <span
-            key={index}
-            style={{ borderBottom: "2px solid #634c89f0" }}
-            className='sequence'
-          >
-            {annotatedPart}
-          </span>
-        );
-      } else if (type === "Bold") {
-        // Handle Bold
-        result.push(
-            <span
-                key={index}
-                style={{ fontWeight: 'bold' }}
-                className='sequence'
-            >
-                {annotatedPart}
-            </span>
-        );
-      } else if (type === "StrikeThrough") {
-          // Handle Bold
-          result.push(
-              <span
-                  key={index}
-                  style={{ textDecoration: 'line-through' }}
-                  className='sequence'
-              >
-                  {annotatedPart}
-              </span>
-          );
-      }
+      result.push(
+        <span
+          key={index}
+          style={{
+            backgroundColor: type[0] === "#" ? type : undefined,
+            borderBottom:
+              type === "Underline" ? "2px solid #634c89f0" : undefined,
+            fontWeight: type === "Bold" ? "bold" : undefined,
+            textDecoration:
+              type === "StrikeThrough" ? "line-through" : undefined,
+          }}
+          className="sequence"
+        >
+          {annotatedPart}
+        </span>
+      );
 
-  
       lastIndex = end;
+      lastType = type;
     });
-  
+
     // Add the remaining unannotated part of the sequence
     result.push(sequence.slice(lastIndex));
     console.log("Annotations: ", formattedAnnotations);
     return result;
   };
+
+  const logSelection = useCallback(() => {
+    const selection = window.getSelection();
+    const isWithinSequence = selection.anchorNode.parentElement.closest(
+      ".sequence-container"
+    );
+    if (!isWithinSequence) return;
+    const selectedText = selection.toString();
+    if (!selectedText) return;
+  
+    const range = selection.getRangeAt(0);
+    const preSelectionRange = range.cloneRange();
+    preSelectionRange.selectNodeContents(document.querySelector(".sequence-container"));
+    preSelectionRange.setEnd(range.startContainer, range.startOffset);
+    const startIndex = preSelectionRange.toString().length;
+  
+    const endIndex = startIndex + selectedText.length;
+  
+    console.log(
+      "Selected sequence:",
+      selectedText,
+      "Start index:",
+      startIndex,
+      "End index:",
+      endIndex
+    );
+  
+    onSequenceSelect(selectedText, startIndex, endIndex);
+  }, [onSequenceSelect]);
   
 
-const logSelection = useCallback(() => {
-  const selection = window.getSelection();
-  const isWithinSequence = selection.anchorNode.parentElement.closest('.sequence-container');
-  if (!isWithinSequence) return;
-  const selectedText = selection.toString();
-  if (!selectedText) return;
-
-  const sequenceContainer = document.querySelector('.sequence-container');
-  const sequenceText = sequenceContainer.textContent;
-  const startIndex = sequenceText.indexOf(selectedText);
-
-  if (startIndex === -1) {
-    console.error('Failed to find start index');
-    return;
-  }
-
-  const endIndex = startIndex + selectedText.length - 1;
-
-  console.log('Selected sequence:', selectedText, 'Start index:', startIndex, 'End index:', endIndex);
-
-  onSequenceSelect(selectedText, startIndex, endIndex);
-}, [onSequenceSelect]);
-
-
-   // JSX for rendering the component
+  // JSX for rendering the component
   return (
     <div>
       <div className="sequence-container" onMouseUp={logSelection}>
