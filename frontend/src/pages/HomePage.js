d2f6e88d18bc9d448da8c041a5e3d10adc2c02b9
vinumaradana <vmaradana@ucsd.edu>
2024-08-30 13:36:54 -0700

    advantages of genographix- home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -15,11 +15,40 @@ function HomePage() {
                 <div className="overlay"></div>
                 <div className="content">
                     <h1 className="title">Welcome to GenoGraphix</h1>
-                    <h2 className="subtitle">Produced by Boolean Lab at UCSD</h2>
+                    <h1 className="subtitle">A free interactive tool to explore, visualize and analyze genetic data.</h1>
                 </div>
             </div>
+            
+            {/* <p className="description2">
+                Provides insights into both DNA and RNA sequences, primer construction, and more. Explore the world of genetics with GenoGraphix, and delve into a user-friendly interface that simplifies complex genetic information for researchers and enthusiasts alike.
+            </p> */}
+        
             <p className="description">
-                GenoGraphix is an amazing tool that helps you visualize and analyze genetic data. It provides insights into both DNA and RNA sequences, primer construction, and more. Explore the world of genetics with GenoGraphix, and delve into a user-friendly interface that simplifies complex genetic information for researchers and enthusiasts alike.
+                Why Choose GenoGraphix?
+            </p>
+
+        
+            <div className="advantages-container">
+                <div className="advantage">
+                    <h3>Comprehensive Analysis</h3>
+                    <p>Get detailed insights into DNA and RNA sequences with our advanced tools.</p>
+                </div>
+                <div className="advantage">
+                    <h3>Easy Primer Design</h3>
+                    <p>Optimize primer design with our intuitive and user-friendly interface.</p>
+                </div>
+                <div className="advantage">
+                    <h3>Efficient Visualization</h3>
+                    <p>Visualize complex genetic data effortlessly with interactive graphics.</p>
+                </div>
+                <div className="advantage">
+                    <h3>Accessible for All</h3>
+                    <p>Whether you're a researcher or enthusiast, our tool is designed for ease of use.</p>
+                </div>
+            </div>
+
+            <p className="description">
+                Explore
             </p>
             <br/>
             <div className="buttons-container">
